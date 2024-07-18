85a49889c4fa90ad5022fee1ca324937ca5438f2
Aryaman Chandrawat <singharyaman357@gmail.com>
2024-07-18 16:54:29 +0530

    intialized login page

---
+++ src/components/Login.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react'
+import {app} from '../config/firebase.config'
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import {FcGoogle} from 'react-icons/fc'
+import { useNavigate } from 'react-router-dom'
+
+
+const Login = ({setAuth}) => {
+
+  const firebaseAuth = getAuth(app)
+  const provider = new GoogleAuthProvider()
+
+  const navigate = useNavigate()
+
+  const loginWithGoogle = async () => {
+    await signInWithPopup(firebaseAuth, provider).then((userCredentialss) => {
+      if(userCredentialss){
+        setAuth(true)
+        window.localStorage.setItem("auth", "true")
+
+        firebaseAuth.onAuthStateChanged((userCredentials) => {
+          if(userCredentials){
+            userCredentials.getIdToken().then((token) => {
+              console.log(token);
+            })
+            navigate("/", {replace : true})
+          }
+          else {
+            setAuth(false)
+            navigate("/login")
+          }  
+        })
+      }
+    })
+  }
+
+  useEffect(() => {
+    if(window.localStorage.getItem("auth") === "true"){
+      navigate("/", {replace : true})
+    }
+  }, [])
+
+  return (
+    <div className='relative w-screen h-screen'>
+        <div className='absolute inset-0 bg-darkOverlay flex items-center justify-center p-4'>
+            <div className='w-full md:w-375 p-4 bg-lightOverlay shadow-2xl rounded-md backdrop-blur-md 
+                flex flex-col items-center justify-center'>
+                <div className='flex items-center gap-2 px-4 py-2 rounded-md bg-cardOverlay cursor-pointer 
+                  hover:bg-card hover:shadow-md duration-100 ease-in-out transition-all'
+                    onClick={loginWithGoogle}>
+                    <FcGoogle className='text-xl'/>
+                    Sign in with Google
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
