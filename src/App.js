b5f1c67e98d1b2e7fb77db85e3cf4167ea8681c0
Aryaman Chandrawat <singharyaman357@gmail.com>
2024-07-18 16:56:00 +0530

    feat: re-routed user with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { Home, Login } from "./components/index";
+import {app} from './config/firebase.config'
+import { getAuth } from 'firebase/auth';
 
-function App() {
+const App = () => {
+
+  const firebaseAuth = getAuth(app)
+  const navigate = useNavigate()
+
+  const [auth, setAuth] = useState(false || window.localStorage.getItem("auth") === "true")
+
+  useEffect(() => {
+    firebaseAuth.onAuthStateChanged((userCredentials) => {
+        if(userCredentials) {
+          userCredentials.getIdToken().then((token) => {
+              console.log(token);
+          })
+        } 
+        else {
+          setAuth(false)
+          window.localStorage.setItem("auth", "false")
+          navigate('/login')
+        }
+    })
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-screen h-screen bg-primary flex justify-center items-center'>
+      <Routes>
+          <Route path='/login' element={<Login setAuth = {setAuth} />}/>
+          <Route path='/*' element={<Home/>}/>
+      </Routes>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
