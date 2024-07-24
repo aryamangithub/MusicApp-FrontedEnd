92f8fc63a539b7ee694f40f4e7c4ac37c40ddfa1
Aryaman Chandrawat <singharyaman357@gmail.com>
2024-07-24 20:59:34 +0530

    feat: added-user-validation-with-API-call

--- src/App.js
+++ src/App.js
@@ -4,23 +4,39 @@ import { Home, Login } from "./components/index";
 import {app} from './config/firebase.config'
 import { getAuth } from 'firebase/auth';
 
+import { AnimatePresence } from 'framer-motion';
+import { validateUser } from './api';
+import {useStateValue} from './context/StateProvider'
+import { actionType } from './context/Reducer';
+
 const App = () => {
 
   const firebaseAuth = getAuth(app)
   const navigate = useNavigate()
 
+  const [{user}, dispatch] = useStateValue()
   const [auth, setAuth] = useState(false || window.localStorage.getItem("auth") === "true")
 
   useEffect(() => {
     firebaseAuth.onAuthStateChanged((userCredentials) => {
         if(userCredentials) {
           userCredentials.getIdToken().then((token) => {
-              console.log(token);
+              // console.log(token);
+              validateUser(token).then((data) => {
+                dispatch({
+                  type : actionType.SET_USER,
+                  user : data
+                })
+              })
           })
         } 
         else {
           setAuth(false)
           window.localStorage.setItem("auth", "false")
+          dispatch({
+            type : actionType.SET_USER,
+            user : null
+          })
           navigate('/login')
         }
     })
@@ -28,13 +44,15 @@ const App = () => {
   
 
   return (
-    <div className='w-screen h-screen bg-primary flex justify-center items-center'>
-      <Routes>
-          <Route path='/login' element={<Login setAuth = {setAuth} />}/>
-          <Route path='/*' element={<Home/>}/>
-      </Routes>
-    </div>
-
+    <AnimatePresence mode='wait'>
+      <div className='h-auto min-w-[680px] bg-primary flex justify-center items-center'>
+        <Routes>
+            <Route path='/login' element={<Login setAuth = {setAuth} />}/>
+            <Route path='/*' element={<Home/>}/>
+        </Routes>
+      </div>
+    </AnimatePresence>
+    
   )
 }
 
