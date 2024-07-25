0ff93d1c039e94ff06eabe4746b7c1ea1022cc80
Aryaman Chandrawat <singharyaman357@gmail.com>
2024-07-25 12:35:23 +0530

    update: added-dashboard-route-path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Routes, useNavigate } from "react-router-dom";
-import { Home, Login } from "./components/index";
+import { Dashboard, Home, Login } from "./components/index";
 import {app} from './config/firebase.config'
 import { getAuth } from 'firebase/auth';
 
@@ -49,6 +49,7 @@ const App = () => {
         <Routes>
             <Route path='/login' element={<Login setAuth = {setAuth} />}/>
             <Route path='/*' element={<Home/>}/>
+            <Route path='/dashboard/*' element={<Dashboard/>}/>
         </Routes>
       </div>
     </AnimatePresence>
